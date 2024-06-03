51570de6c765bddd7c069a24c6eb4cda34cec5ce
Eduardo Valim <eduardovalim@me.com>
2024-06-03 19:02:41 -0400

    update file paths in scripts

--- src/contrastive/abtbuy/run_pretraining_clean_roberta.sh
+++ src/contrastive/abtbuy/run_pretraining_clean_roberta.sh
@@ -8,20 +8,20 @@ BATCH=$1
 LR=$2
 TEMP=$3
 AUG=$4
-python run_pretraining_deepmatcher.py \
+CUDA_VISIBLE_DEVICES=0 python run_pretraining_deepmatcher.py \
     --do_train \
 	--dataset_name=abt-buy \
 	--clean=True \
-    --train_file /your_path/contrastive-product-matching/data/processed/abt-buy/contrastive/abt-buy-train.pkl.gz \
-	--id_deduction_set /your_path/contrastive-product-matching/data/interim/abt-buy/abt-buy-train.json.gz \
+    --train_file ../../contrastive-product-matching/data/processed/abt-buy/contrastive/abt-buy-train.pkl.gz \
+	--id_deduction_set ../../contrastive-product-matching/data/interim/abt-buy/abt-buy-train.json.gz \
 	--tokenizer="roberta-base" \
 	--grad_checkpoint=True \
-    --output_dir /your_path/contrastive-product-matching/reports/contrastive/abtbuy-clean-$AUG$BATCH-$LR-$TEMP-roberta-base/ \
+    --output_dir ../../contrastive-product-matching/reports/contrastive/abtbuy-clean-$AUG$BATCH-$LR-$TEMP-roberta-base/ \
 	--temperature=$TEMP \
 	--per_device_train_batch_size=$BATCH \
 	--learning_rate=$LR \
 	--weight_decay=0.01 \
-	--num_train_epochs=5 \
+	--num_train_epochs=200 \
 	--lr_scheduler_type="linear" \
 	--warmup_ratio=0.05 \
 	--max_grad_norm=1.0 \
